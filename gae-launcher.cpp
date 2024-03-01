9cfa014b0a1bd11cc1ff83568502da7ca27bfeb5
Aniruddh155 <129205099+Aniruddh155@users.noreply.github.com>
2024-03-01 09:46:59 +0530

    gae-launcher.cpp

--- gae-launcher.cpp
+++ gae-launcher.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 int main(){
-    cout<<"Balaparameshwar"<<endl;
-    cout<<"21BCS5544"<<endl;
+    cout<<"Aniruddh Nagar"<<endl;
+    cout<<"21BCS5499"<<endl;
     cout<<"21BCS-CC-620-A"<<endl;
-    cout<<"mark = 28/30?"<<endl;
     return 0;
 }
